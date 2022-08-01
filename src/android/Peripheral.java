1b10ce7bb9fb32356ca6a86d96dc6af32e171e10
CoeOmnichannel <73339509+CoeOmnichannel@users.noreply.github.com>
2022-08-01 17:04:53 +0100

    Update Peripheral.java

--- src/android/Peripheral.java
+++ src/android/Peripheral.java
@@ -17,7 +17,7 @@ package com.megster.cordova.ble.central;
 import android.app.Activity;
 
 import android.bluetooth.*;
-import NotifyBufferContainer;
+import NotifyBufferContainer.*;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
