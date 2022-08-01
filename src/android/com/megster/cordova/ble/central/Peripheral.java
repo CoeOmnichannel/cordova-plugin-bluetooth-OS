b8cfaf8780cda069325f996ece0d1dab2920bc2b
CoeOmnichannel <73339509+CoeOmnichannel@users.noreply.github.com>
2022-08-01 17:43:26 +0100

    Update Peripheral.java

--- src/android/com/megster/cordova/ble/central/Peripheral.java
+++ src/android/com/megster/cordova/ble/central/Peripheral.java
@@ -39,7 +39,40 @@ import java.util.concurrent.atomic.AtomicBoolean;
 /**
  * Peripheral wraps the BluetoothDevice and provides methods to convert to JSON.
  */
-public class Peripheral extends BluetoothGattCallback {
+private class Peripheral extends BluetoothGattCallback {
+	
+	private class NotifyBufferContainer {
+
+    public final Integer maxBufferSize;
+    private Integer bufferCount;
+    public ByteBuffer items;
+
+    public NotifyBufferContainer(Integer size) {
+        this.maxBufferSize = size;
+        this.resetBuffer();
+    }
+    public void resetBuffer(){
+        this.bufferCount = 0;
+        this.items = ByteBuffer.allocate(this.maxBufferSize);
+    }
+    public void put(byte[] value){
+        this.bufferCount += value.length;
+        if (this.items.remaining() < value.length) {
+            return;
+        }
+        this.items.put(value);
+    }
+    public boolean isBufferFull(){
+        return this.bufferCount >= this.maxBufferSize;
+    }
+    public Integer size() {
+        return this.bufferCount;
+    }
+    @Override
+    protected void finalize() throws Throwable {
+        this.items = ByteBuffer.allocate(0);
+    }
+}
 
     // 0x2902 org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
     //public final static UUID CLIENT_CHARACTERISTIC_CONFIGURATION_UUID = UUID.fromString("00002902-0000-1000-8000-00805F9B34FB");
